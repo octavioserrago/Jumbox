ef8a8a6ab4596567bb6b5baa434ee0b8cca0d35e
octavioserrago <octavioserrago.dev@gmail.com>
2023-11-23 23:37:37 -0300

    Initiated sales feature implementation. In progress. Checkpoint commit.

--- src/IU/PantallaHome.java
+++ src/IU/PantallaHome.java
@@ -37,7 +37,7 @@ public class PantallaHome extends JFrame {
 	 */
 	public PantallaHome() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 539, 364);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -45,11 +45,11 @@ public class PantallaHome extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Productos");
-		btnNewButton.setBounds(252, 21, 117, 29);
+		btnNewButton.setBounds(184, 50, 117, 29);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Pedidos");
-		btnNewButton_1.setBounds(252, 62, 117, 29);
+		btnNewButton_1.setBounds(261, 169, 117, 29);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Proveedores");
@@ -57,11 +57,11 @@ public class PantallaHome extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_2.setBounds(252, 103, 117, 29);
+		btnNewButton_2.setBounds(338, 50, 117, 29);
 		contentPane.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Camiones");
-		btnNewButton_3.setBounds(252, 144, 117, 29);
+		btnNewButton_3.setBounds(184, 103, 117, 29);
 		contentPane.add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("Cerrar Sesion");
@@ -69,21 +69,34 @@ public class PantallaHome extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_4.setBounds(252, 209, 117, 29);
+		btnNewButton_4.setBounds(228, 246, 117, 29);
 		contentPane.add(btnNewButton_4);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("/Users/octavioserrago/Documents/Da Vinci/3er cuatri/Programacion Avanzada/Jumbox/src/img/jumbox-grande.png"));
-		lblNewLabel.setBounds(72, 66, 100, 107);
+		lblNewLabel.setBounds(42, 50, 100, 107);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Jumbox");
-		lblNewLabel_1.setBounds(82, 174, 61, 16);
+		lblNewLabel_1.setBounds(63, 153, 61, 16);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Mayorista");
-		lblNewLabel_2.setBounds(105, 194, 61, 16);
+		lblNewLabel_2.setBounds(52, 174, 61, 16);
 		contentPane.add(lblNewLabel_2);
+		
+		JButton btnNewButton_5 = new JButton("Ventas");
+		btnNewButton_5.setBounds(338, 103, 117, 29);
+		contentPane.add(btnNewButton_5);
+		
+		btnNewButton_5.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		        
+		    	PantallaVenta ventaFrame = new PantallaVenta();
+		        ventaFrame.setVisible(true);
+		        dispose();
+		    }
+		});
 		
 		
 		btnNewButton.addActionListener(new ActionListener() {
---
+++ src/IU/PantallaVenta.java
@@ -0,0 +1,74 @@
+package IU;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class PantallaVenta extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PantallaVenta frame = new PantallaVenta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PantallaVenta() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 586, 381);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Cargar Venta");
+		btnNewButton.setBounds(252, 107, 117, 29);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Filtrar ventas");
+		btnNewButton_1.setBounds(252, 164, 117, 29);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Volver");
+		btnNewButton_2.setBounds(235, 276, 117, 29);
+		contentPane.add(btnNewButton_2);
+		
+		btnNewButton_2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                PantallaHome pantallaHome = new PantallaHome();
+                pantallaHome.setVisible(true);
+                dispose();
+            }
+        });
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("/Users/octavioserrago/Documents/Da Vinci/3er cuatri/Programacion Avanzada/Jumbox/src/img/jumbox-grande.png"));
+		lblNewLabel.setBounds(112, 107, 100, 100);
+		contentPane.add(lblNewLabel);
+	}
+}
